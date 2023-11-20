fbec12f8c7fc8cc10e2c2885e358f09e498894d6
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-20 18:34:03 -0300

    feat: add functions for validating body objects
    Add a middleware for validating request bodies and a function
    for validating data structures for the web sockets.

---
+++ src/middlewares/validateBody/index.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import Ajv, { JSONSchemaType } from 'ajv';
+
+const validateSchema = (data: unknown, schema: JSONSchemaType<unknown>) => {
+  const ajv = new Ajv();
+  const validate = ajv.compile(schema);
+
+  if (validate(data)) return true;
+  return false;
+};
+
+/**
+ * High order function that returns a middleware validating request body
+ * against a predefined schema. If request body is invalid, throws an error.
+ * Can be used for validating request bodies.
+ * @param schema The predefined schema
+ * @returns The middleware
+ */
+export const validateReqBody = (schema: JSONSchemaType<unknown>) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (validateSchema(req.body, schema)) {
+      next();
+    } else {
+      throw new Error('Invalid body');
+    }
+  };
+};
+
+/**
+ * Function that validate body against a predefined schema.
+ * If request body is invalid, throws an error.
+ * Can be used for validating socket bodies.
+ * @param schema The predefined schema
+ * @returns The middleware
+ */
+export const validateBody = (
+  data: unknown,
+  schema: JSONSchemaType<unknown>
+) => {
+  if (!validateSchema(data, schema)) throw new Error('Invalid body');
+};
