0c6653069829849f226922c8bd36f8b8d3ba07a2
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-23 09:02:59 -0300

    feat: add room router and error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,9 @@ import indexRouter from "./routes/index";
 import chatRouter from "./routes/chat";
 import { chatSetup } from "./chat/chat";
 import { Server } from "socket.io";
-
 import userRouter from "./routes/userRoutes";
+import roomRouter from './routes/roomRoutes';
+import { expressErrorHandler } from './middlewares/errorHandlers/expressErrorHandler';
 
 const app = express();
 const port = 3000;
@@ -14,12 +15,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
 app.use('/chat', chatRouter);
+
 chatSetup(new Server(
   app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   }))
 );
+
 app.use("/user", userRouter);
+app.use('/', indexRouter);
+app.use('/room', roomRouter);
+
+app.use(expressErrorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
