53077ee39d673ce4f47042d2c17dc4cd51441307
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-23 09:04:48 -0300

    feat: add validation schema for join game room req

---
+++ src/middlewares/validateBody/schemas/gameJoinRoomSchema.ts
@@ -0,0 +1,11 @@
+import { IJoinGameRoomInfo } from '../../../interfaces/GameInterfaces';
+import { JSONSchemaType } from 'ajv';
+
+export const gameJoinRoomSchema: JSONSchemaType<IJoinGameRoomInfo> = {
+  type: 'object',
+  properties: {
+    roomId: { type: 'string' },
+    team: { type: 'string', enum: ['teamA', 'teamB'] },
+  },
+  required: ['roomId', 'team'],
+};
