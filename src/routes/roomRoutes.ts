3280c9f159f0b05202e6946e2bd8f0f3026008ea
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-23 09:05:39 -0300

    feat: create game room routes

---
+++ src/routes/roomRoutes.ts
@@ -0,0 +1,13 @@
+import { gameJoinRoomSchema } from '../middlewares/validateBody/schemas/gameJoinRoomSchema';
+import { gameRoomSchema } from '../middlewares/validateBody/schemas/gameRoomSchema';
+import { validateReqBody } from '../middlewares/validateBody';
+import roomController from '../controllers/roomController';
+import express, { Router } from 'express';
+
+const router: Router = express.Router();
+
+router.post('/', validateReqBody(gameRoomSchema), roomController.create);
+router.get('/:id', roomController.get);
+router.patch('/', validateReqBody(gameJoinRoomSchema), roomController.join);
+
+export default router;
