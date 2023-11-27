dd21cbd1b3daa9d78dc3743be0d5dacbb46b459f
Jose Szychowski <jszychowski.laba@solvd.com>
2023-11-27 10:14:17 -0300

    fix: conflict between ports
    Fixed a port conflict where chat feature was not correctly
    attached to the http server, causing a port conflict

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ import { authenticateToken } from './middlewares/auth/authMiddleware';
 import { gameSetup } from './routes/socketRoutes/gameRoutes';
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,20 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 app.use('/chat', chatRouter);
 app.use('/user', userRouter);
-app.use('/room', roomRouter);
-app.use("/", indexRouter);
-app.use("/chat", chatRouter);
+app.use('/room', authenticateToken, roomRouter);
+app.use('/', indexRouter);
+app.use('/chat', chatRouter);
 
-chatSetup(
-  new Server(
-    app.listen(process.env.CHAT_PORT, () => {
-      console.log(`Chat server is running on http://localhost:${process.env.CHAT_PORT}`);
-    })
-  )
-);
+const PORT = Number(process.env.PORT) || 3000;
 
-app.use("/user", userRouter);
-app.use("/room", authenticateToken, roomRouter);
+app.use('/user', userRouter);
+app.use('/room', authenticateToken, roomRouter);
 
 app.use(expressErrorHandler);
 
@@ -43,6 +36,6 @@ const io = new Server(server);
 chatSetup(io);
 gameSetup(io);
 
-server.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
