204b0260dea5bcb47dfcfbb25e6ceed05fd9e7d9
Francesca Pavan <francescapavan7620@gmail.com>
2023-12-02 14:59:30 -0300

    [fix] End game if team has 1 player when turn begins

--- src/services/game/disconnect.ts
+++ src/services/game/disconnect.ts
@@ -11,23 +11,25 @@ export const disconnect = (socket: Socket) => {
     const user = onlineUsers.get(socket.id);
     
     if (user) {
+      // Obtain roomId from the the user information
+      const gameState = rooms.get(user.roomId);
+      if (gameState) {
+        console.log(socket.id);
+        // Delete user from the gameRoom in the team he is playing
+        if ( gameState.teamAPlayers.has(socket.id)) { 
+          gameState.teamAPlayers.delete(socket.id);
+        } else { 
+          gameState.teamBPlayers.delete(socket.id);
+        }
+        rooms.set(user.roomId, gameState);
+      } 
+      
       // send message to everyone that user left the room
       socket
         .to(user.roomId)
         .emit('user-left', `${user.username} has left the room`);
       socket.leave(user.roomId);
       onlineUsers.delete(socket.id);
-
-      // Obtain roomId from the the user information
-      const gameState = rooms.get(user.roomId);
-      if (gameState) {
-        // Delete user from the gameRoom in the team he is playing
-        if ( socket.id in gameState.teamAPlayers)
-          gameState.teamAPlayers.delete(socket.id);
-        else 
-          gameState.teamBPlayers.delete(socket.id);
-        rooms.set(user.roomId, gameState);
-      }      
     }
   };
 
